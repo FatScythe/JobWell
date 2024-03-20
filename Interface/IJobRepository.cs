80e7723240d30f432091a37aa0cf8b794ec44577
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-03-20 21:45:09 +0100

    Added UpdateJobDto class

--- Interface/IJobRepository.cs
+++ Interface/IJobRepository.cs
@@ -1,13 +1,14 @@
-﻿using server.Models;
+﻿using server.Dto.Job;
+using server.Models;
 
 namespace server.Interface
 {
     public interface IJobRepository
     {
-        Task<List<Job>> GetAllJobsAsync();
-        Task<Job> GetJobByIdAsync(int jobId);
-        Task<Job> CreateJob(Job job);
-        Task<Job> UpdateJobAsync(int Job, Job job);
+        Task<List<Job>> GetAllJobs();
+        Task<Job> GetJobById(int jobId);
+        Task<Job> CreateJob(CreateJobDto job);
+        Task<Job> UpdateJob(int Job, UpdateJobDto job);
         Task<Job> DeleteJobById(int jobId);
     }
 }
