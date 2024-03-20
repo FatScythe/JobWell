83f16de8024bf44cacd30231d823d91a57006b81
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-03-20 21:41:44 +0100

    Working on Application methods

--- Interface/IApplicationRepository.cs
+++ Interface/IApplicationRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace server.Interface
+﻿using server.Models;
+
+namespace server.Interface
 {
     public interface IApplicationRepository
     {
+        Task<Application> GetApplications();
+        Task<List<Application>> GetApplicationById(int applicationId);
+        Task<Application> CreateApplication(Application application);
+        Task<Application> UpdateApplicationId(Application application);
+        Task<Application> DeleteApplicationById(int applicationId);
     }
 }
--- Repository/ApplicationRepository.cs
+++ Repository/ApplicationRepository.cs
@@ -1,6 +1,33 @@
-﻿namespace server.Repository
+﻿using server.Interface;
+using server.Models;
+
+namespace server.Repository
 {
-    public class ApplicationRepository
+    public class ApplicationRepository : IApplicationRepository
     {
+        public Task<Application> CreateApplication(Application application)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Application> DeleteApplicationById(int applicationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Application>> GetApplicationById(int applicationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Application> GetApplications()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Application> UpdateApplicationId(Application application)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
