25af0e5412ad6771147f60f80eed17a80cb372a4
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-03-27 11:44:49 +0100

    Added Logout Ctrller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using server.Models;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class AuthController: ControllerBase
+    {
+        private readonly SignInManager<Account> _signInManager;
+        public AuthController(SignInManager<Account> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("logout")]
+
+        public async Task<ActionResult> LogoutAccount([FromForm] object empty)
+        {
+            if (empty != null)
+            {
+                await _signInManager.SignOutAsync();
+                return Ok(new { isSuccess = true, message = "Logout successful" });
+            }
+            return Unauthorized(new { isSuccess = false, message = "Logout failed" });
+        }
+    }
+}
