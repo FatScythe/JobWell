8eecee2ae417bc45ccb4c5e85c64a1b1f939a76c
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-03-29 23:29:34 +0100

    Feat: Implemented RBA for Post, Put and Delete Routes

--- Controllers/JobControllers.cs
+++ Controllers/JobControllers.cs
@@ -1,13 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using server.Dto.Job;
 using server.Extensions;
 using server.Interface;
 using server.Mappers;
 using server.Models;
-using System.Security.Claims;
+using System.Data;
 
 namespace server.Controllers
 {
@@ -16,90 +15,85 @@ namespace server.Controllers
     public class JobControllers: ControllerBase
     {
         private readonly IJobRepository _jobRepo;
-        private readonly UserManager<Account> _userManager;
-        public JobControllers(IJobRepository jobRepository, UserManager<Account> userManager)
+        public JobControllers(IJobRepository jobRepository)
         {
             _jobRepo = jobRepository;
-            _userManager = userManager;
         }
 
         [HttpGet]
-        [Authorize]
         public async Task<ActionResult<List<Job>>> GetJobs()
         {
             var jobs = await _jobRepo.GetAllJobs();
-            var userInfo = User.GetCurrentUserInfo();
-            var userId = _userManager.GetUserId(User);  
 
-            return Ok(new { isSuccess = true, jobs, userInfo, userId });
+            return Ok(new { success = true, jobs});
         }
 
         [HttpGet("{id:int}")]
         public async Task<ActionResult<Job>> GetJob(int id)
         {
             if (id <= 0)
-                return BadRequest(new {isSuccess = false, message= "Please provide a valid job id"});
+                return BadRequest(new {success = false, message= "Please provide a valid job id"});
 
             var job = await _jobRepo.GetJobById(id);
 
             if (job is null)
-                return NotFound(new { isSuccess = false, message = $"Job with id {id} was not found" });
+                return NotFound(new { success = false, message = $"Job with id {id} was not found" });
 
 
-            return Ok(new {isSuccess = true, job });
+            return Ok(new {success = true, job });
         }
 
         [HttpPost]
-        [Authorize]
+        [Authorize(Roles = "Admin, Employer")]
         public async Task<IActionResult> CreateJob(CreateJobDto createJob)
         {
             if (createJob is null)
-               return BadRequest(new { isSuccess = false, message = "Please provide fields" });
+               return BadRequest(new { success = false, message = "Please provide fields" });
 
             if (!ModelState.IsValid)
-                return BadRequest(new { isSuccess = false, message = "Please fill all fields", error = ModelState });
+                return BadRequest(new { success = false, message = "Please fill all fields", error = ModelState });
 
             var job = await _jobRepo.CreateJob(createJob.ToJobFromCreateDto());
 
             if (job is null)
-                return StatusCode(500, new { isSuccess = false, message = "Something went wrong" });
+                return StatusCode(500, new { success = false, message = "Something went wrong" });
 
-            return CreatedAtAction(nameof(GetJob), new { id = job.Id }, new { isSuccess = true, job });
+            return CreatedAtAction(nameof(GetJob), new { id = job.Id }, new { success = true, job });
         }
 
         [HttpPut("{id:int}")]
-        [Authorize]
+        [Authorize(Roles = "Admin, Employer")]
         public async Task<IActionResult> UpdateJob(int id,[FromBody] UpdateJobDto updateJob)
         {
             if (id <= 0)
-                return BadRequest(new { isSuccess = false, message = "Please provide a valid job id" });
+                return BadRequest(new { success = false, message = "Please provide a valid job id" });
 
             if (updateJob is null)
-                return BadRequest(new { isSuccess = false, message = "Please provide update fields" });
+                return BadRequest(new { success = false, message = "Please provide update fields" });
 
             if (!ModelState.IsValid)
-                return BadRequest(new { isSuccess = false, message = "Please fill all fields", error = ModelState });
+                return BadRequest(new { success = false, message = "Please fill all fields", error = ModelState });
 
             var job = await _jobRepo.UpdateJob(id, updateJob);
 
             if (job is null)
-                return NotFound(new { isSuccess = false, message = $"Job with id {id} was not found" });
+                return NotFound(new { success = false, message = $"Job with id {id} was not found" });
 
-            return Ok(new {isSuccess = true, message = "Job updated", job });
+            return Ok(new {success = true, message = "Job updated", job });
         }
 
-        [HttpDelete("{id:int}"), Authorize]
+        [HttpDelete("{id:int}"), Authorize(Roles = "Admin, Employer")]
         public async Task<IActionResult> DeleteJob([FromRoute] int id)
         {
             if (id <= 0)
-                return BadRequest(new { isSuccess = false, message = "Please provide a valid job id" });
+                return BadRequest(new { success = false, message = "Please provide a valid job id" });
 
             var job = await _jobRepo.DeleteJobById(id);
 
             if (job is null)
-                return NotFound(new { isSuccess = false, message = $"Job with id {id} was not found" });
+                return NotFound(new { success = false, message = $"Job with id {id} was not found" });
 
-            return Ok(new { isSuccess = true, message = "Job deleted", job });
+            return Ok(new { success = true, message = "Job deleted", job });
         }
     }
 }
