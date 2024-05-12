4c09c16b04ebd8c814269e61a1ac2fa087950fbd
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-05-12 17:25:26 +0100

    chore: Added mappers to convert updateDto to ApplicationDto

---
+++ Mappers/ApplicationMapper.cs
@@ -0,0 +1,24 @@
+﻿using server.Dto.Application;
+using server.Models;
+
+namespace server.Mappers
+{
+    public static class ApplicationMapper
+    {
+        public static Application ToApplicationFromUpdateAppDto (this UpdateApplicationDto updateApplicationDto)
+        {
+            return new Application 
+            {
+                FirstName = updateApplicationDto.FirstName,
+                MiddleName = updateApplicationDto.MiddleName,
+                LastName = updateApplicationDto.LastName,
+                Email = updateApplicationDto.Email,
+                Mobile = updateApplicationDto.Mobile,
+                Gender = updateApplicationDto.Gender,
+                Address = updateApplicationDto.Address,
+                Resume = updateApplicationDto.Resume,
+                DoB = DateOnly.Parse(updateApplicationDto.DoB)
+            };
+        } 
+    }
+}
