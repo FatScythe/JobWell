5e6300864bbea689a486c824c966e15e93830597
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-05-12 17:41:05 +0100

    chore: added dto for application update

--- Dto/Application/UpdateApplicationDto.cs
+++ Dto/Application/UpdateApplicationDto.cs
@@ -1,16 +1,15 @@
 ﻿namespace server.Dto.Application
 {
-    public class CreateApplicationDto
+    public class UpdateApplicationDto
     {
         public string FirstName { get; set; }
         public string MiddleName { get; set; } = string.Empty;
         public string LastName { get; set; }
-        public string Email { get; set; } = string.Empty;
+        public string Email { get; set; }
         public string Mobile { get; set; }
         public string Gender { get; set; }
         public string DoB { get; set; }
         public string Address { get; set; }
         public string Resume { get; set; }
-        public int JobId { get; set; }
     }
 }
